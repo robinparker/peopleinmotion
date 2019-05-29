4ee0bc5a2730b8b1ea8cbd9ad26600cbadac198a
Robin Parker <robinkristianparker@gmail.com>
2019-05-29 13:42:41 +0100

    Added Skeleton CSS for grid

--- header.php
+++ header.php
@@ -16,7 +16,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+	<link href="<?php echo get_template_directory_uri(); ?>/skeleton.css" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
